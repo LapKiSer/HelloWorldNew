018fe329fb83cf8cb53c48209937ba4029037a7d
LapKiSer <lapkovskiykirill@gmail.com>
2024-07-27 16:59:46 +0300

    Initial Commit

---
+++ HelloWorldNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldNew
+//
+//  Created by Кирилл Лапковский on 27.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
